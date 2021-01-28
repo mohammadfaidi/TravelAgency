c15ecbecbb731d436daf4548441a63972918fc3d
mohammadfaidi <31220054+mohammadfaidi@users.noreply.github.com>
2021-01-28 17:26:34 +0200

    Add files via upload

---
+++ untitled1/bin/untitled1.dart
@@ -0,0 +1,5 @@
+import 'package:untitled1/main.dart' as untitled1;
+
+void main(List<String> arguments) {
+  print('Hello world: ${untitled1.calculate()}!');
+}
---
+++ untitled1/test/untitled1_test.dart
@@ -0,0 +1,8 @@
+import 'package:untitled1/main.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
